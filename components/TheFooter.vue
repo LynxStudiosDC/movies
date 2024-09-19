8ff385c0fca4210023583bfbfc19dfc7e12962f2
LynxStudios <108672750+LynxStudiosDC@users.noreply.github.com>
2024-09-18 22:58:27 -0400

    Update TheFooter.vue

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -11,7 +11,7 @@
         Made with
       </div>
       <a href="https://lynxstudios.xyz" target="_blank" title="LynxStudios">
-        < Lynx Studios h5 />
+        <Lynx Studios h5 />
       </a>
     </div>
     <div flex="~ row gap2" items-center>
